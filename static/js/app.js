5e2b88a2a967694550e9de649e628ee32ad13f97
gabprado <gabprado@hotmail.com>
2020-03-18 00:32:25 -0500

    add gauge and format code

--- static/js/app.js
+++ static/js/app.js
@@ -1,77 +1,61 @@
 function buildMetadata(sample) {
-  let metadata = d3.select("#sample-metadata")
-  metadata.html("")
-  d3.json("/metadata/"+sample).then((data) => {
-    Object.entries(data).forEach(([key,value]) => {
-      metadata.append("p").text(`${key} : ${value}`)
-    })
-  })
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+  let metadata = d3.select("#sample-metadata");
+  metadata.html("");
+  d3.json("/metadata/" + sample).then((data) => {
+      Object.entries(data).forEach(([key, value]) => {
+          metadata.append("p").text(`${key} : ${value}`)
+      })
+      buildGauge(data.WFREQ)
+  });
 }
 
 function buildCharts(sample) {
-
-//Bubble chart
-  d3.json("/samples/"+sample).then((sample_data) => {
-  let trace1 = {
-    x: sample_data.otu_ids,
-    y: sample_data.sample_values,
-    mode: 'markers',
-    text: sample_data.otu_labels,
-    marker: {
-      color: sample_data.otu_ids,
-      size: sample_data.sample_values,
-      colorscale: "Viridis"
-    }
-  };
-  let data = [trace1];
-  let layout = {
-    showlegend: false,
-    height: 600,
-    width: 1400
-  };
-  Plotly.newPlot("bubble", data, layout);
-  //Pie Chart
-  data = [{
-    values: sample_data.sample_values.slice(0,10),
-    labels: sample_data.otu_ids.slice(0,10),
-    hovertext: sample_data.otu_labels.slice(0,10),
-    type: "pie"
-  }];
-  layout = {
-    height: 600,
-    width: 450
-  };
-  Plotly.newPlot("pie", data, layout);
-})
+  //Bubble chart
+  d3.json("/samples/" + sample).then((sample_data) => {
+      let trace1 = {
+          x: sample_data.otu_ids,
+          y: sample_data.sample_values,
+          mode: "markers",
+          text: sample_data.otu_labels,
+          marker: {
+              color: sample_data.otu_ids,
+              size: sample_data.sample_values,
+              colorscale: "Viridis"
+          }
+      };
+      let data = [trace1];
+      let layout = {
+          showlegend: false,
+      };
+      Plotly.newPlot("bubble", data, layout);
+      //Pie Chart
+      data = [{
+          values: sample_data.sample_values.slice(0, 10),
+          labels: sample_data.otu_ids.slice(0, 10),
+          hovertext: sample_data.otu_labels.slice(0, 10),
+          type: "pie"
+      }];
+      Plotly.newPlot("pie", data);
+  });
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
+  let selector = d3.select("#selDataset");
   d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
+      sampleNames.forEach((sample) => {
+          selector
+              .append("option")
+              .text(sample)
+              .property("value", sample);
+      });
+      const firstSample = sampleNames[0];
+      buildCharts(firstSample);
+      buildMetadata(firstSample);
   });
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
-
-// Initialize the dashboard
 init();
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,1 +1,70 @@
-
+function buildGauge(sample) {
+    let level = sample;
+    let degrees = 180 - ((level * 20)),
+        radius = .8;
+    let radians = degrees * Math.PI / 180;
+    let aX = 0.025 * Math.cos((degrees - 90) * Math.PI / 180);
+    let aY = 0.025 * Math.sin((degrees - 90) * Math.PI / 180);
+    let bX = -.025 * Math.cos((degrees - 90) * Math.PI / 180);
+    let bY = -0.025 * Math.sin((degrees - 90) * Math.PI / 180);
+    let cX = radius * Math.cos(radians);
+    let cY = radius * Math.sin(radians);
+    let path = `M ${aX} ${aY} L ${bX} ${bY} L ${cX} ${cY} Z`;
+    let data = [{
+            type: "scatter",
+            x: [0],
+            y: [0],
+            marker: {
+                size: 28,
+                color: "850000",
+            },
+            showlegend: false,
+            text: level,
+            name: "speed",
+            hoverinfo: ("text+name")
+        },
+        {
+            values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+            rotation: 90,
+            text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            textinfo: "text",
+            textposition: "inside",
+            marker: {
+                colors: ["#0284A1", "#0297B8", "#22A4C1", "#41B1CA", "#61BED3", "#81CBDB", "#A0D8E4", "#C0E5ED", "#DFF2F6", "#494949"]
+            },
+            labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            hoverinfo: "label",
+            hole: .5,
+            type: "pie",
+            showlegend: false
+        }
+    ];
+    let layout = {
+        height: 400,
+        width: 400,
+        shapes: [{
+            type: "path",
+            path: path,
+            fillcolor: "850000",
+            line: {
+                color: "850000"
+            }
+        }],
+        title: "Belly Button Washing Frequency<br>Scrubs Per Week",
+        xaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            fixedrange: true,
+            range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            fixedrange: true,
+            range: [-1, 1]
+        }
+    };
+    Plotly.newPlot("gauge", data, layout);
+}
